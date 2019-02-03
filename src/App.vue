d3eadfb193064200dfd82151770e64ce28ff58a6
Aman <rumman.djuice1@gmail.com>
2019-02-03 18:30:35 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input class="form-control" v-model="user.userName">
+                </div>
+                <div class="form-group">
+                    <label>Mail</label>
+                    <input class="form-control" v-model="user.userEmail">
+                </div>
+                <button class="btn btn-primary" v-on:click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" v-on:click="fetchData">Get Data</button><br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="x in fetchedUser">{{x.userName}}--{{x.userEmail}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -10,6 +24,22 @@
 
 <script>
     export default {
+        data(){
+            return{
+                user:{userName:'', userEmail:''},
+                fetchedUser:[],
+            };
+        },
+        methods:{
+            submit(){
+                this.$http.post('https://vuejs-http-4e9e1.firebaseio.com/AmanData.json', this.user).then(response=>{ console.log(response) }, error=>{ console.log(error) });
+            },
+            fetchData(){
+                this.$http.get('https://vuejs-http-4e9e1.firebaseio.com/AmanData.json').then(response=>{ return response.json(); })
+                    .then(receivedData=>{ const resultArr=[];
+                                          for(let key)});
+            }
+        }
     }
 </script>
 
